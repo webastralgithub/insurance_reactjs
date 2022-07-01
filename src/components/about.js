486d12012bde70d0c14b251405300817b5f47326
webastral.development <webastral.development@gmail.com>
2022-07-01 19:22:25 +0530

    change hero section

--- src/components/about.js
+++ src/components/about.js
@@ -8,17 +8,17 @@ const About=()=>{
     return(
  <div id='about' className='scroll-break'>
     <section className='py-12'>
-        <div className='container m-auto'>
-            <div className='grid grid-cols-2  items-center'>
-            <div className='col-span-2 sm:col-span-1'>
-            <div>
+        <div className='container px-5 m-auto'>
+            <div className='grid grid-cols-2 lg:gap-12 md:gap-8 sm:gap-4 items-center'>
+            <div className='col-span-2 sm:col-span-1 mb-5'>
+            <div className='about-img-head'>
                 <img className='w-full' src='../images/about-banner.jpg'/>
             </div>
             </div>
             <div className='col-span-2 sm:col-span-1'>
             <div className='about-content'>
                <h4 className='text-4xl mb-5 font-medium uppercase'>ABOUT US</h4>
-               <p className='text-xl text-justify text-gray-100'>Our insurance professionals provide personalised services on a client to client basis. We are able to survey Canada's leading insurance companies and advise you without being biased by any one companies services. A meeting with one of our professionals will demystify the insurance process and create a plan that works for you.</p>
+               <p className='text-xl text-justify'>Our insurance professionals provide personalised services on a client to client basis. We are able to survey Canada's leading insurance companies and advise you without being biased by any one companies services. A meeting with one of our professionals will demystify the insurance process and create a plan that works for you.</p>
             </div>
             </div>
             </div>
--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -7,11 +7,22 @@ import React from 'react';
 const HeroSection=()=>{
     return(
  <div id='home' className='scroll-break'>
-    <section className='mt-[100px]'>
-    <div className='relative'>
-    <img className='w-full' src='../images/banner-bg-5.jpeg'/>
-    <p className='absolute caption lg:text-7xl md:text-5xl sm:text-3xl text-2xl'>Insurance</p>
-    <a href='#form' type="button" className="inline-block md:px-6 sm:px-4 px-3 md:py-2.5 py-1.5 bg-blue-600 text-white font-medium md:text-xl sm:text-base text-sm leading-tight uppercase rounded shadow-md hero-btn absolute hero-btn">Get in Touch</a>
+    <section className='mt-[100px] hero-bg lg:py-24 md:py-20 sm:py-16 py-12 '>
+    <div className='container m-auto px-5'>
+      <div className='grig grid-cols-8'>
+         <div className='col-span-4'>
+         <div className=' caption bg-white rounded-xl lg:px-[40px] px-[20px] lg:py-[30px] py-[15px]'>
+    <p className='  lg:text-5xl md:text-4xl sm:text-3xl text-2xl mb-4'>Insurance</p>
+    <p className='text-white lg:text-xl mb-5 '>You can feel more confident by protecting what’s important to you.</p>
+    <a href='#form' type="button" className="inline-block md:px-6 sm:px-4 px-3 md:py-2.5 py-1.5 bg-blue-600 text-white font-medium md:text-xl sm:text-base text-sm leading-tight uppercase rounded shadow-md hero-btn  hero-btn">Get in Touch</a>
+    </div>
+         </div>
+         <div className='col-span-4'>
+            
+            </div>
+      </div>
+   
+   
     </div>   
     </section>
  </div>
